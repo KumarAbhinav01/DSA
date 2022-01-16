c42a5dba3ee4cc1fc4f9f7e59c0a2953f233af9f
KumarAbhinav01 <abhinav.bitd@gmail.com>
2022-01-16 22:32:32 +0530

    Homepage Update

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -5,31 +5,28 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('../../static/img/h1.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        This repository contains all of my progress while learning DS. I have tried to note down as much information as possible and I hope it will benifit others.
       </>
     ),
   },
   {
     title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('../../static/img/h2.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        My goal behind this project was making it a complete <code>open source</code> and believe you will also contribute to this documentation and share your knowledge.
       </>
     ),
   },
   {
     title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('../../static/img/h3.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        This is a documentation website built using Docusaurus and powered with React. Docusaurus is great for open source documentation projects and I love using it a lot.
       </>
     ),
   },
